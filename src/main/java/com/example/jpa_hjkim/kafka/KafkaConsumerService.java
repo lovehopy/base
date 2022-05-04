83493e45aff0e56bd105f2658ff9a058ce339e8b
hjkim <lovehopy1@gmail.com>
2022-05-04 17:30:45 +0900

    change

--- src/main/java/com/example/jpa_hjkim/kafka/KafkaConsumerService.java
+++ src/main/java/com/example/jpa_hjkim/kafka/KafkaConsumerService.java
@@ -2,6 +2,7 @@ package com.example.jpa_hjkim.kafka;
 
 import lombok.AllArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,15 @@ public class KafkaConsumerService {
     private SimpMessagingTemplate simpMessagingTemplate;
 
     @KafkaListener(topics="hjkim", groupId = "hjkim-pc")
+    @MessageMapping("/message")
     public void consume(String msg){
         simpMessagingTemplate.convertAndSend("/publish/message", msg);
         System.out.println("received ::: " +  msg);
+    }
+
+    @MessageMapping(value = "/message/enter")
+    public void enter() {
+        System.out.println("연결성공");
+        simpMessagingTemplate.convertAndSend("/publish/message" , "채팅방에 참여하셨습니다.");
     }
 }
